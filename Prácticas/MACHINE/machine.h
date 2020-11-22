3dc15f4c333307fb3e4a85a327bfea16c7001c16
lore <lorelizgodinez@gmail.com>
2020-11-21 18:17:57 -0600

    final machine

---
+++ Prácticas/MACHINE/machine.h
@@ -0,0 +1,24 @@
+// definitions 
+#define glass 1788 //gr
+#define PINACOLADA_TIME 	1000000
+#define MARGARITA_TIME 		200000
+#define SANGRIA_TIME 	    800000
+// data types 
+typedef unsigned char uint8;
+typedef unsigned short uint16;
+typedef unsigned int uint32;
+
+typedef enum
+{
+	enIce = 0,
+	enPineapplejuice,
+	enEvaporatedmilk,
+	enCoconutcream,
+	enTequila,
+	enLemonjuice,
+	enRedwine,
+	enOrangejuice,
+	enFruts,
+	enSoda,
+	Delay
+}tenStates;
